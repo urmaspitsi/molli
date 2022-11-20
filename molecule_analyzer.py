ba25f2a6bd465b38596866b324d011208e326af0
Urmas Pitsi <urmaspitsi@gmail.com>
2022-11-20 14:15:20 +0200

    group by initial version

--- molecule_analyzer.py
+++ molecule_analyzer.py
@@ -1,5 +1,5 @@
 import copy
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from datetime import datetime
 import json
 import numpy as np
@@ -13,6 +13,7 @@ from ase import Atoms
 from ase import constraints as ase_constraints
 from ase.constraints import FixInternals
 
+import features as ft
 
 # Dataset: List[Path]
 # Add: file path, List[file path], directory path (incl. sub dirs), dir path list
@@ -49,5 +50,61 @@ from ase.constraints import FixInternals
 #   conformers: crest, crest-reoptimized
 #
 #
+# Molecules and Features pairs: (mol, feature)?
+# Feature: over many molecules: for m in mols: calc_feature_value
 #
+#
+#
+#
+@dataclass
+class SeriesItem():
+  name: str
+  labels: List[str] = field(default_factory=list)
+  values: List[float] = field(default_factory=list)
+
+
+def calculate_values_groupby_features(
+                                      molecules: List[Atoms],
+                                      features_list: List[ft.Feature]
+                                      ) -> List[SeriesItem]:
+
+  res = []
+ 
+  labels = [mol.info["name"] for mol in molecules]
+
+  for ft in features_list:
+    features_values = [ft.calculate_value(mol) for mol in molecules]
+
+    series_result = SeriesItem(
+                              name=ft.label,
+                              labels=labels,
+                              values=features_values
+                              )
+
+    res.append(series_result)
+
+  return res
+
+
+def calculate_values_groupby_molecules(
+                                      molecules: List[Atoms],
+                                      features_list: List[ft.Feature]
+                                      ) -> List[SeriesItem]:
+
+  res = []
+ 
+  labels = [ft.label for ft in features_list]
+
+  for mol in molecules:
+    features_values = [ft.calculate_value(mol) for ft in features_list]
+
+    series_result = SeriesItem(
+                              name=mol.info["name"],
+                              labels=labels,
+                              values=features_values
+                              )
+
+    res.append(series_result)
+
+  return res
 
