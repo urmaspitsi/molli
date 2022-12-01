540459070ac5a9ab56c775f9dd2161300149f011
Urmas Pitsi <urmaspitsi@gmail.com>
2022-12-01 16:54:04 +0200

    gauss

--- gaussian_tasks.py
+++ gaussian_tasks.py
@@ -6,6 +6,7 @@ from typing import Any, Dict, List, Tuple, Union
 
 import gaussian_utils as GU
 import utils as ut
+import xyz_parser
 
 
 #%%
@@ -21,19 +22,20 @@ import utils as ut
 #xyz_file = Path("C:/tmp/gaussian/input/mol24_ex15_gfnff_crest_conformers.xyz")
 #xyz_file = Path("C:/tmp/gaussian/input/mol24_ex23_gfn2_crestconfs_from_ex16_crest3.xyz")
 #xyz_file = Path("C:/tmp/gaussian/input/mol24_ex21_gfnff_crestconfs_from_ex16_crest5.xyz")
-xyz_file = Path("C:/tmp/gaussian/input/mol24_ex0a_gfn2_crestconfs.xyz")
+xyz_file = Path("C:/tmp/gaussian/input/mol24_ex16_gfn2_crest_conformers.xyz")
 
 gaussian_jobs_output_dir = Path("C:/tmp/gaussian/test_jobs") #
 
 #    "#T PBE1PBE/Def2SVPP/SVPFit opt=(calcfc,maxcycles=10) formcheck",
 #    "#T BP86/Def2SVPP/SVPFit opt=(calcfc,maxcycles=10) formcheck",
+#    "#T BP86/Def2SVPP/SVPFit opt=(calcfc,maxcycles=100) formcheck",
 #    "#T BP86/STO-3G opt=(calcfc,maxcycles=25) formcheck",
 #    "#T PBE1PBE/STO-3G opt=(calcfc,maxcycles=25) formcheck",
 
 lines_to_prepend = [
-    "#T PBE1PBE/STO-3G opt=(calcfc,maxcycles=25) formcheck",
+    "#T BP86/Def2SVPP/SVPFit opt=(calcfc,maxcycles=100) formcheck",
     "",
-    "mol24_ex0a_gfn2_crestconfs",
+    "mol24_ex16_gfn2_crestconfs",
     "",
     "0 1",
   ]
@@ -43,9 +45,14 @@ gaussian_job_files = GU.create_gaussian_job_files_from_xyz_steps(
                             output_dir=gaussian_jobs_output_dir,
                             lines_before_xyz_coords=lines_to_prepend,
                             step_nrs_to_write=list(range(1, 62)),
-                            job_file_name_prefix="mol24_ex0a_gfn2_pbe1pbe_sto3g_crest_"
+                            job_file_name_prefix="mol24_ex16_gfn2_bp86_def2svpp_svpfit_crest_"
                           )
 
+#
+# top5 from ex0a_BP86_STO3G: 13,29,23,6,26
+# top5 from ex0a_BP86_Def2SVPP_SVPFit: 10,1,13,9,20
+# combined top5: [1,6,9,10,13,20,23,26,29]
+#
 
 #%%
 ############################################################################
@@ -53,23 +60,30 @@ gaussian_job_files = GU.create_gaussian_job_files_from_xyz_steps(
 # Case 1: Many log files in one folder
 ############################################################################
 
+# bp86_sto3g
+# bp86_def2svpp_svpfit
+# pbe1pbe_def2svpp_svpfit
+# pbe1pbe_sto3g
+
 log_files_dirs = [
-    Path("C:/tmp/gaussian/crest_reoptimize/crest_mol24_ex0a_PBE1PBE_STO3G_results"),
+#    Path("C:/tmp/gaussian/crest_reoptimize/crest_mol24_ex0a_s1_BP86_Def2SVPP_SVPFit_results"),
+    Path("C:/tmp/gaussian/jobs_results/mol24_PBE1PBE_Def2SVPP_SVPFit_results"),
 #    Path("C:/tmp/gaussian/jobs_juhan/jobs_juhan_results"),
 
   ]
 
 gaussian_log_files = ut.get_file_paths_in_many_dirs(log_files_dirs, ".log")
 
+output_dir = Path("C:/tmp/gaussian/jobs_results/mol24_PBE1PBE_Def2SVPP_SVPFit_logs")
 #output_dir = Path("C:/tmp/gaussian/jobs_juhan/jobs_juhan_logs")
-output_dir = "C:/tmp/gaussian/crest_reoptimize/crest_mol24_ex0a_PBE1PBE_STO3G_logs"
+#output_dir = "C:/tmp/gaussian/crest_reoptimize/crest_mol24_ex0a_s1_BP86_Def2SVPP_SVPFit_logs"
 #output_dir = "C:/tmp/gaussian/jobs_logs_bp86_def2svpp_svpfit"
 #output_dir = "C:/tmp/gaussian/jobs_logs_pbe1pbe_def2svpp_svpfit"
 #output_dir = log_files_dirs[0]
 
 #aggregate_log_file_name = "aggregate_log.txt"
 #aggregate_log_file_name = "ex15_bp86_sto3g_step10_log.txt"
-aggregate_log_file_name = "ex0a_pbe1pbe_sto3g_log.txt"
+aggregate_log_file_name = "mol24_pbe1pbe_def2svpp_svpfit_log.txt"
 #aggregate_log_file_name = "bp86_def2svpp_svpfit_log.txt"
 #aggregate_log_file_name = "pbe1pbe_def2svpp_svpfit_log.txt"
 
@@ -158,3 +172,30 @@ len(xyz_lines)
 # %%
 xyz_lines
 # %%
+
+#%%
+############################################################################
+# Collect xyz files into one single file
+############################################################################
+
+dir1 = Path("C:/tmp/gaussian/crest_reoptimize/crest_mol24_ex0a_s1_BP86_Def2SVPP_SVPFit_logs")
+idxs = [1,6,9,10,13,20,23,26,29]
+
+list_of_input_path_and_idx_tuples = [
+#  (dir1.joinpath(f"mol24_ex0a_gfn2_bp86_def2svpp_svpfit_crest_{i}.xyz") , "all") for i in idxs
+  (dir1.joinpath(f"mol24_ex0a_gfn2_bp86_def2svpp_svpfit_crest_{i}_last_step.xyz") , "all") for i in idxs
+  ]
+
+#output_path = Path("C:/tmp/gaussian/test_jobs/mol24_ex0a_gfn2_crestconfs.xyz")
+output_path = Path("C:/tmp/gaussian/test_jobs/mol24_ex0a_gfn2_crestconfs_reopt_bp86_def2svpp_svpfit.xyz")
+
+xyz_parser.write_xyz_files_into_one(
+    list_of_input_path_and_idx_tuples=list_of_input_path_and_idx_tuples,
+    output_path=output_path
+  )
+
+
+
+
+# %%
+
