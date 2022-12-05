c674cd3861d584db5e2cf030e7c84e94265203fe
Urmas Pitsi <urmaspitsi@gmail.com>
2022-12-05 15:09:04 +0200

    traj ex

--- trajectory_example.py
+++ trajectory_example.py
@@ -1,55 +1,4 @@
-# %%
-trajectories_dir = Path("C:/tmp/gaussian/optimization_trajectories/mol24_ex16_crest5")
-num_steps = 5
 
-base_trajectory = MultiItemFileSource(
-    file_path=trajectories_dir.joinpath("mol24_ex16_crest5_PBE1PBE_cc_pVTZ_TZVPFit_opt_steps.xyz"),
-    name="pbe1pbe_cc_pvtz_tzvpfit",
-    item_idxs=num_steps
-  )
-
-trajectories_to_compare = [
-    MultiItemFileSource(
-      file_path=trajectories_dir.joinpath("mol24_ex16_gfn2_bp86_sto3g_crest_5_opt_steps.xyz"),
-      name="bp86_sto3g",
-      item_idxs=num_steps
-      ),
-
-    MultiItemFileSource(
-      file_path=trajectories_dir.joinpath("mol24_ex16_gfn2_bp86_def2svpp_svpfit_crest_5_opt_steps.xyz"),
-      name="bp86_def2svpp_svpfit",
-      item_idxs=num_steps
-      ),
-
-    MultiItemFileSource(
-      file_path=trajectories_dir.joinpath("crest_mol24_ex16_crest5_PBE1PBE_Def2SVPP_SVPFit_opt_steps.xyz"),
-      name="pbe1pbe_def2svpp_svpfit",
-      item_idxs=num_steps
-      ),
-  ]
-
-metric_funcs = [
-  (ms.mad_of_positions, "mean_abs_diff"),
-  (ms.rmsd_of_positions, "root_mean_of_sq_diffs"),
-]
-
-mol24_ex16_crest5_traj_analyzer = TrajectoryAnalyzer(
-    description="mol24_ex16_crest5 optimization trajectories",
-    base_trajectory=base_trajectory,
-    trajectories_to_compare=trajectories_to_compare,
-    metric_functions=metric_funcs,
-  )
-
-
-# %%
-trajectories_dir = Path("C:/tmp/gaussian/optimization_trajectories/mol24_ex16_crest5")
-num_steps = 5
-
-base_trajectory = MultiItemFileSource(
-    file_path=trajectories_dir.joinpath("mol24_ex16_crest5_PBE1PBE_cc_pVTZ_TZVPFit_opt_steps.xyz"),
-    name="pbe1pbe_cc_pvtz_tzvpfit",
-    item_idxs=num_steps
-  )
 #%%
 from pathlib import Path
 from typing import Any, Dict, List, Tuple, Union
@@ -69,12 +18,14 @@ metric_funcs = [
   (ms.rmsd_of_positions, "root_mean_of_sq_diffs"),
 ]
 
+
 #%%
 ###############################################################################
 # mol24_ex16_crest5
 ###############################################################################
 
 trajectories_dir = Path("C:/tmp/gaussian/optimization_trajectories/mol24_ex16_crest5")
+num_steps = 5
 
 base_trajectory = MultiItemFileSource(
     file_path=trajectories_dir.joinpath("mol24_ex16_crest5_PBE1PBE_cc_pVTZ_TZVPFit_opt_steps.xyz"),
@@ -108,10 +59,6 @@ mol24_ex16_crest5_traj_analyzer = TrajectoryAnalyzer(
     trajectories_to_compare=trajectories_to_compare,
     metric_functions=metric_funcs,
   )
-
-# %%
-mol24_ex16_crest5_traj_analyzer.metrics["mean_abs_diff"]["base_to_start"]
-
 
 # %%
 ###############################################################################
@@ -210,11 +157,26 @@ mol24_ex0a_crest10_traj_analyzer = TrajectoryAnalyzer(
 ###############################################################################
 #
 ###############################################################################
+# %%
+traj_analyzer = mol24_ex16_crest5_traj_analyzer
+#traj_analyzer = mol24_ex19_crest23_traj_analyzer
+#traj_analyzer = mol24_ex0a_crest10_traj_analyzer
+
+#%%
+#traj_analyzer.metrics["mean_abs_diff"]["base_to_start"]
+traj_analyzer.metrics["mean_abs_diff"]["to_base"]
+
+#%%
+mol24_ex16_crest5_traj_analyzer.metrics["mean_abs_diff"]["to_base"]
+
+#%%
+mol24_ex19_crest23_traj_analyzer.metrics["mean_abs_diff"]["to_base"]
+
+#%%
+mol24_ex0a_crest10_traj_analyzer.metrics["mean_abs_diff"]["to_base"]
 
 #%%
 
 #%%
-
 #%%
-
-
+#%%
