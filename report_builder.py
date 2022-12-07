8f7ab61e27fcbeaeec6d86d338355b0ef0a3f9cd
Urmas Pitsi <urmaspitsi@gmail.com>
2022-12-07 20:53:30 +0200

    report builder

---
+++ report_builder.py
@@ -0,0 +1,85 @@
+# Creates dataset from optimisation results
+
+from copy import deepcopy
+from datetime import datetime
+from itertools import chain
+import numpy as np
+import pandas as pd
+from pathlib import Path
+from typing import Any, Dict, List, Tuple, Union
+
+import ase_utils as au
+import utils as ut
+
+
+def read_aggregate_summary_to_dict(
+          input_path: Union[str, Path]
+        ) -> Dict:
+
+  '''
+    Reads aggregate summary that is generated from gaussian log files.
+    Return Pandas DataFrame object.
+  '''    
+
+  data = ut.read_json_file_to_dict(file_path=input_path)
+
+  columns_from_experiment = [
+    "input_path",
+    ]
+
+  columns_from_results = [
+    "final_xyz",
+    "opt_steps"
+    ]
+
+  columns_from_scf_summary = [
+    "gaussian_command",
+    "dft_functional",
+    "optimization_converged",
+    "elapsed_time_str",
+    "elapsed_time_minutes",
+    "num_steps",
+    "minutes_per_step",
+    "energy_start",
+    "energy_end",
+    "energy_delta",
+    "energy_delta_text",
+    ]
+
+  columns = columns_from_scf_summary + columns_from_experiment + columns_from_results
+
+  res = {col: [] for col in columns }
+
+  for ex in data["experiments"]:
+    for col in columns_from_experiment:
+      if col in ex:
+        res[col].append(ex[col])
+      else:
+        res[col].append(None)
+
+    for col in columns_from_results:
+      if col in ex["results"]:
+        res[col].append(ex["results"][col])
+      else:
+        res[col].append(None)
+
+    for col in columns_from_scf_summary:
+      if col in ex["results"]["scf_summary"]:
+        res[col].append(ex["results"]["scf_summary"][col])
+      else:
+        res[col].append(None)
+
+  return res
+
+
+def read_aggregate_summary_to_dataframe(
+          input_path: Union[str, Path]
+        ) -> pd.DataFrame:
+  '''
+    Reads aggregate summary that is generated from gaussian log files.
+    Return Pandas DataFrame object.
+  '''    
+  return pd.DataFrame(read_aggregate_summary_to_dict(input_path=input_path))
+
+
+
