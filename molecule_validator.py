f89dd71dce46ebdc49256147eed101db9f1d2d6b
Urmas Pitsi <urmaspitsi@gmail.com>
2022-12-08 00:26:39 +0200

    validate_with_target_molecule_bonds

--- molecule_validator.py
+++ molecule_validator.py
@@ -7,6 +7,7 @@ from ase import Atoms
 import ase_utils as au
 import features as ft
 from features_constraints import Constraint, FeatureConstraint, MoleculeFeatureConstraints
+from target_molecule import TargetMoleculeBonds
 
 
 def validate_structure(
@@ -32,4 +33,44 @@ def target_mol_constraints() -> List[FeatureConstraint]:
 
 
   ]
+
+
+def validate_with_target_molecule_bonds(mol: Atoms) -> Dict:
+  target_cc = au.convert_bonds_to_set(TargetMoleculeBonds.CC)
+  mol_cc = au.get_bonds_as_set(mol=mol, element1="C", element2="C")
+  missing_cc_in_target, missing_cc_in_mol = au.get_bonds_diff(bonds1=target_cc, bonds2=mol_cc)
+
+  target_ch = au.convert_bonds_to_set(TargetMoleculeBonds.CH)
+  mol_ch = au.get_bonds_as_set(mol=mol, element1="C", element2="H")
+  missing_ch_in_target, missing_ch_in_mol = au.get_bonds_diff(bonds1=target_ch, bonds2=mol_ch)
+
+  target_co = au.convert_bonds_to_set(TargetMoleculeBonds.CO)
+  mol_co = au.get_bonds_as_set(mol=mol, element1="C", element2="O")
+  missing_co_in_target, missing_co_in_mol = au.get_bonds_diff(bonds1=target_co, bonds2=mol_co)
+
+  total_missing_in_target = len(missing_cc_in_target) + len(missing_ch_in_target) + len(missing_co_in_target)
+  total_missing_in_mol = len(missing_cc_in_mol) + len(missing_ch_in_mol) + len(missing_co_in_mol)
+  is_ok = total_missing_in_target + total_missing_in_mol == 0
+
+  res = {
+      "is_valid": is_ok,
+      "cc": {
+              "new_cc_bonds": list(missing_cc_in_target),
+              "lost_cc_bonds": list(missing_cc_in_mol),
+              },
+
+      "ch": {
+              "new_ch_bonds": list(missing_ch_in_target),
+              "lost_ch_bonds": list(missing_ch_in_mol),
+              },
+
+      "co": {
+              "new_co_bonds": list(missing_co_in_target),
+              "lost_co_bonds": list(missing_co_in_mol),
+              },
+    }
+
+  return res
+
+
 
