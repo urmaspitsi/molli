1f3b0e9b066d821d6dad07c125d18ae421194df0
Urmas Pitsi <urmaspitsi@gmail.com>
2022-12-07 23:35:37 +0200

    mol val

--- molecule_validator.py
+++ molecule_validator.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass, field
 import numpy as np
 from typing import Any, Dict, List, Tuple, Union
 
@@ -5,7 +6,30 @@ from ase import Atoms
 
 import ase_utils as au
 import features as ft
+from features_constraints import Constraint, FeatureConstraint, MoleculeFeatureConstraints
 
 
-def validate_structure(mol: Atoms, constraints: List[Constraints]) -> bool:
-  return True
+def validate_structure(
+                        mol: Atoms,
+                        constraints: List[FeatureConstraint]
+                      ) -> bool:
+
+  return MoleculeFeatureConstraints(
+              label="",
+              molecule=mol,
+              feature_constraints=constraints
+            ).satisfied()
+
+
+
+def target_mol_constraints() -> List[FeatureConstraint]:
+  return [
+    FeatureConstraint(
+        label="",
+        feature=ft.Angle("", 0, 0, 0),
+        constraint=Constraint("", "<", 0)
+      )
+
+
+  ]
+
