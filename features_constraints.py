b51373416cb62585f509c491ecfb63b6a894153f
Urmas Pitsi <urmaspitsi@gmail.com>
2022-12-07 21:25:12 +0200

    features constraints

---
+++ features_constraints.py
@@ -0,0 +1,51 @@
+from dataclasses import dataclass, field
+from pathlib import Path
+from typing import Any, Dict, List, Set, Tuple, Union
+import numpy as np
+
+from ase import Atoms
+import features as ft
+import metrics as ms
+
+
+@dataclass
+class Constraint():
+  label: str
+  value: float
+  condition: str
+  abs_tolerance: float = 0.0000001
+
+  def satisfied(self, test_with_value: float) -> bool:
+    if self.condition == "<":
+      return test_with_value < self.value
+    elif self.condition == ">":
+      return test_with_value > self.value
+    elif self.condition == "<=":
+      return test_with_value <= self.value
+    elif self.condition == ">=":
+      return test_with_value >= self.value
+    elif self.condition == "=":
+      return abs(self.value - test_with_value) < self.abs_tolerance
+
+
+@dataclass
+class FeatureConstraint():
+  label: str
+  feature: ft.Feature
+  constraint: Constraint
+
+
+@dataclass
+class MoleculeFeatureConstraints():
+  label: str
+  molecule: Atoms
+  feature_constraints: List[FeatureConstraint]
+
+  def satisfied(self) -> bool:
+    mol = self.molecule
+
+    for fc in self.feature_constraints:
+      if not fc.constraint.satisfied(test_with_value=fc.feature.calculate_value(mol)):
+        return False
+
+    return True
---
+++ molecule_validator.py
@@ -0,0 +1,11 @@
+import numpy as np
+from typing import Any, Dict, List, Tuple, Union
+
+from ase import Atoms
+
+import ase_utils as au
+import features as ft
+
+
+def validate_structure(mol: Atoms, constraints: List[Constraints]) -> bool:
+  return True
