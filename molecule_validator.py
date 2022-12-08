fb66c56b3f2987f190e49731feefb92606600d08
Urmas Pitsi <urmaspitsi@gmail.com>
2022-12-08 21:49:46 +0200

    missing bonds info

--- molecule_validator.py
+++ molecule_validator.py
@@ -7,7 +7,7 @@ from ase import Atoms
 import ase_utils as au
 import features as ft
 from features_constraints import Constraint, FeatureConstraint, MoleculeFeatureConstraints
-from target_molecule import TargetMoleculeBonds
+from target_molecule import TargetMoleculeBonds as tm
 
 
 def validate_structure(
@@ -34,19 +34,28 @@ def target_mol_constraints() -> List[FeatureConstraint]:
 
   ]
 
+def get_missing_bonds_info(
+                            mol: Atoms,
+                            bonds: List[Tuple[int, int]]
+                          ) -> List[str]:
+
+  res = [f"{x}, dist_in_target={round(tm.bonds_distances[x], 3)}, dist_in_mol={round(mol.get_distance(x[0], x[1]),3)}" for x in bonds]
+
+  return res
+
 
 def validate_with_target_molecule_bonds(mol: Atoms) -> Dict:
-  target_cc = au.convert_bonds_to_set(TargetMoleculeBonds.CC)
+  target_cc = au.convert_bonds_to_set(tm.CC)
   mol_cc = au.get_bonds_as_set(mol=mol, element1="C", element2="C")
   missing_cc_in_target, missing_cc_in_mol = au.get_bonds_diff(bonds1=target_cc, bonds2=mol_cc)
   num_missing_cc = len(missing_cc_in_target) + len(missing_cc_in_mol)
 
-  target_ch = au.convert_bonds_to_set(TargetMoleculeBonds.CH)
+  target_ch = au.convert_bonds_to_set(tm.CH)
   mol_ch = au.get_bonds_as_set(mol=mol, element1="C", element2="H")
   missing_ch_in_target, missing_ch_in_mol = au.get_bonds_diff(bonds1=target_ch, bonds2=mol_ch)
   num_missing_ch = len(missing_ch_in_target) + len(missing_ch_in_mol)
 
-  target_co = au.convert_bonds_to_set(TargetMoleculeBonds.CO)
+  target_co = au.convert_bonds_to_set(tm.CO)
   mol_co = au.get_bonds_as_set(mol=mol, element1="C", element2="O")
   missing_co_in_target, missing_co_in_mol = au.get_bonds_diff(bonds1=target_co, bonds2=mol_co)
   num_missing_co = len(missing_co_in_target) + len(missing_co_in_mol)
@@ -67,22 +76,22 @@ def validate_with_target_molecule_bonds(mol: Atoms) -> Dict:
     res["C-O"] = {}
 
   if len(missing_cc_in_target) > 0:
-    res["C-C"]["new_cc_bonds"] = list(missing_cc_in_target)
+    res["C-C"]["new_cc_bonds"] = get_missing_bonds_info(mol, missing_cc_in_target)
 
   if len(missing_cc_in_mol) > 0:
-    res["C-C"]["lost_cc_bonds"] = list(missing_cc_in_mol)
+    res["C-C"]["lost_cc_bonds"] = get_missing_bonds_info(mol, missing_cc_in_mol)
 
   if len(missing_ch_in_target) > 0:
-    res["C-H"]["new_ch_bonds"] = list(missing_ch_in_target)
+    res["C-H"]["new_ch_bonds"] = get_missing_bonds_info(mol, missing_ch_in_target)
 
   if len(missing_ch_in_mol) > 0:
-    res["C-H"]["lost_ch_bonds"] = list(missing_ch_in_mol)
+    res["C-H"]["lost_ch_bonds"] = get_missing_bonds_info(mol, missing_ch_in_mol)
 
   if len(missing_co_in_target) > 0:
-    res["C-O"]["new_co_bonds"] = list(missing_co_in_target)
+    res["C-O"]["new_co_bonds"] = get_missing_bonds_info(mol, missing_co_in_target)
 
   if len(missing_co_in_mol) > 0:
-    res["C-O"]["lost_co_bonds"] = list(missing_co_in_mol)
+    res["C-O"]["lost_co_bonds"] = get_missing_bonds_info(mol, missing_co_in_mol)
 
   return res
 
